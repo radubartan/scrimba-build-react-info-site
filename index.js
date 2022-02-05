42e8f0b45cccbda50ae91c2058bc24c6f618a0cc
Radu Bartan <radu.bartan@gmail.com>
2022-02-04 19:17:34 -0500

    L4 - First React Practice - Cleveland Browns Linebackers

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+ReactDOM.render(
+   <div>
+      <h2>Cleveland Browns Linebackers</h2>
+      <ul>
+         <li>Jeremiah Owusu-Koramoah</li>
+         <li>Anthony Walker</li>
+         <li>Sione Takitaki</li>
+         <li>Malcolm Smith</li>
+         <li>Mack Wilson</li>
+         <li>Jacob Phillips</li>
+         <li>Tony Fields II</li>
+      </ul>
+   </div>, 
+   document.getElementById("root"));
+
+
 
