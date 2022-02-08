229a0d3bb236d10f4f787ea2f6564df9dd7fc499
Radu Bartan <radu.bartan@gmail.com>
2022-02-08 17:04:15 -0500

    L14 - Custom Components part 2 - my work

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-import ReactDOM from "react-dom"
+import reactDOM from "react-dom"
+import logo from "../scrimba-build-react-info-site/images/logo_React_v3.png"
 
-function Why_I_am_excited_to_learn_React() {
+function Header() {
    return (
       <>
-         <h3>Why I am excited to learn React</h3>
+         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" target="_blank" href="https://reactjs.org/docs/getting-started.html">
+               <img src={logo} width="40" className="d-inline-block align-top" />
+               Why I'm excited to learn React
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+               <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+               <ul className="navbar-nav">
+                  <li className="nav-item active">
+                     <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+                  </li>
+                  <li className="nav-item">
+                     <a className="nav-link" href="#">Features</a>
+                  </li>
+                  <li className="nav-item">
+                     <a className="nav-link" href="#">Pricing</a>
+                  </li>
+                  <li className="nav-item">
+                     <a className="nav-link disabled" href="#">Disabled</a>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+      </>
+   )
+}
+
+function MainContent() {
+   return (
+      <>
+         <h3>Reasons I like to rean React</h3>
          <ol>
             <li>React will blend my strength in front end design with my desire to build full stack apps</li>
             <li>I will get to implement JavaScript & React UI goodies seen on CodePen</li>
@@ -15,9 +48,32 @@ function Why_I_am_excited_to_learn_React() {
    )
 }
 
-ReactDOM.render(<Why_I_am_excited_to_learn_React />, document.getElementById('root'))
+function Footer() {
+   return (
+      <>
+         <p className="text-muted"><em>© 2022 Bartan Software Development. All rights reserved.</em></p>
+      </>
+   )
+}
+
+reactDOM.render(
+   <div>
+      <Header />
+      <MainContent />
+      <Footer />
+   </div>,
+   document.getElementById("root")
+)
 
 
 
 
 
+// <h3>Why I am excited to learn React</h3>
+// <ol>
+//    <li>React will blend my strength in front end design with my desire to build full stack apps</li>
+//    <li>I will get to implement JavaScript & React UI goodies seen on CodePen</li>
+//    <li>Using React makes it exciting to be on the cutting edge of front end technologies</li>
+//    <li>At some point I want to build SASS businesses based on the React sites I build</li>
+//    <li>I will get to work professionally on website UIs - which is what I want to do</li>
+// </ol>
