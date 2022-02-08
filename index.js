f5af90d8ff99eb1442417f75f70eeaa8503c2522
Radu Bartan <radu.bartan@gmail.com>
2022-02-08 17:43:43 -0500

    L15 - Custom Components Quiz - my answers

--- index.js
+++ index.js
@@ -26,3 +26,35 @@ function Page() {
 ReactDOM.render(<Page />, document.getElementById("root"))
 
 
+{/*
+   My answers to the 'Custom Components Quiz'
+
+   What is a React component?
+	> a component allows us to write code that can be executed over and over again
+	> a function that returns React elements is considered a component in React
+
+
+	What's wrong with this code?
+		function myComponent() {
+		    return (
+		        <small>I'm tiny text!</small>
+		    )
+		}
+	> the function name has camelCase not PascalCase
+	 
+	
+	What's wrong with this code?
+		function Header() {
+		    return (
+		        <header>
+		            <nav>
+		                <img src="./react-logo.png" width="40px" />
+		            </nav>
+		        </header>
+		    )
+		}
+		ReactDOM.render(Header(), document.getElementById("root"))
+	> the ReactDOM.render does not have the JSX function call:
+      ReactDOM.render(<Header />, document.getElementById("root"))
+
+*/}
