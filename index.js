a86ee05156f72fb421ec883d3c5753d8bf5349ac
Radu Bartan <radu.bartan@gmail.com>
2022-02-08 08:28:35 -0500

    L13 - Custom Components - cloned to C:\ drive

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function TemporaryName() {
             <li>Has well over 100K stars on GitHub</li>
             <li>Is maintained by Facebook</li>
             <li>Powers thousands of enterprise apps, including mobile apps</li>
+            <li>Excellent cross platform support</li>
          </ul>
       </>
    )
