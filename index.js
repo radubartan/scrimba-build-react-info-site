41bb4d468309abe7f842284927fb7a1759b9352f
Radu Bartan <radu.bartan@gmail.com>
2022-02-09 12:08:59 -0500

    L17 - Styling with Classes - added Google Fonts

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function Header() {
 function MainContent() {
    return (
       <div className="mainContent">
-         <h1>Reasons I'm excited to learn React</h1>
-         <ol>
+         <h1 className="font_FiraSansCondensed_extraBold800">Reasons I'm excited to learn React</h1>
+         <ol className="font_FiraSansCondensed_regular400">
             <li>It's a popular library, so I'll be
                able to fit in with the cool kids!</li>
             <li>I'm more likely to get a job as a developer
