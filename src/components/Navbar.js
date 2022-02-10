cbff5def1378f962003b916642b7e61328099c59
Radu Bartan <radu.bartan@gmail.com>
2022-02-10 11:55:24 -0500

    L24 - Navbar & Styling - my code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
+import logo from '../images/react-icon-small.png'
+
 function Navbar() {
    return (
-      <h1>Navbar component</h1>
+      <nav className='nav d-flex flex-row'>
+         <div className='logo'><img src={logo}/></div>
+         <div className='reactFacts'>ReactFacts</div>
+         <div className='reactCourse'>React Course - Project 1</div>
+      </nav>
    )
 }
 
