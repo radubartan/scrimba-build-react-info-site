b2869e29cdc000740fdaef9d9676cb1e277f5280
Radu Bartan <radu.bartan@gmail.com>
2022-02-10 13:06:16 -0500

    L25 - Main Section - my code

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,17 @@
-function Main() {
+export default function Main() {
    return (
-      <h1>Main component</h1>
+      <div className="mainContainer">
+         <div className="funFacts">Fun facts about React</div>
+         <ul className="listFacts">
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+         </ul>
+      </div>
    )
 }
 
-export default Main
 
 
