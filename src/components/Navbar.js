b6218f7f513741454cdd331d3a0f3e36296d1744
Radu Bartan <radu.bartan@gmail.com>
2022-02-11 13:59:02 -0500

    L27 - Add background logo - moved pics to Cloudinary

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,15 @@
-import logo from '../images/react-icon-small.png'
+import React from "react";
 
-function Navbar() {
+export default function Navbar() {
    return (
       <nav>
-         <img className="nav--icon" src={logo}/>
+         <img className="nav--icon" src='https://res.cloudinary.com/analogyofpearl/image/upload/v1644605550/Shared/scrimba-build-react-info-site/react-icon-small.png'/>
          <h3 className="nav--logo_text">ReactFacts</h3>
          <h4 className="nav--title">React Course - Project 1</h4>
       </nav>
    )
 }
 
-export default Navbar
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from "react"
 import ReactDOM from "react-dom"
 import '../src/css/style.css'
 import App from "../src/App.js"
